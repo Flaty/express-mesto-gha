0a252d90e537e660144948cbe5d31afebbce884b
kaneki <hitomi.jpeg@gmail.com>
2023-04-06 12:40:48 +0300

    more minor fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken');
-const AuthErr = require('../errors/AuthErr');
+const AuthorizationError = require('../errors/authorization-errors');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthErr('Необходима авторизация');
+    throw new AuthorizationError('Необходима авторизация');
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return next(new AuthErr('Необходима авторизация'));
+    return next(new AuthorizationError('Необходима авторизация'));
   }
   req.user = payload;
   next();
